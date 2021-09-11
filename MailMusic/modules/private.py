a700e2963f1ca4c9b3794e7205a8fa8e0b432e32
W29F <81409844+W29F@users.noreply.github.com>
2021-09-12 05:49:26 +0700

    Update private.py

--- MailMusic/modules/private.py
+++ MailMusic/modules/private.py
@@ -18,7 +18,7 @@ import logging
 from MailMusic.modules.msg import Messages as tr
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from MailMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
+from MailMusic.config import ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']))
